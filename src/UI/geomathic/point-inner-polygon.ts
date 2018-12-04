d3758fcfea2a24d271deac22c7cd11d99c292174
yaochenghao <yaochenghao@bytedance.com>
2018-12-04 13:43:41 +0800

    add: point-in-polygon geometric

---
+++ src/UI/geomathic/point-inner-polygon.ts
@@ -0,0 +1,59 @@
+/**
+ * @description judge point is inner the Polygon
+ * ray-cross 算法
+ */
+
+import { IPoint, IPointList, isIPoint } from '@/interface/point';
+import { isArray } from 'util';
+
+export default (p: IPoint, polygon: IPointList): boolean => {
+    let countNum: number = 0;
+    const point: IPoint = p;
+    const pointList: IPointList = polygon;
+
+    for (let i: number = 0; i < pointList.length; i = i + 1) {
+        const p1: IPoint = pointList[i];
+        const p2: IPoint = pointList[(i + 1) % pointList.length];
+
+        // 平行线
+        if (p1.y === p2.y) {
+            continue;
+        }
+
+        // 最高点都不到该点的
+        if (Math.max(p1.y, p2.y) < point.y) {
+            continue;
+        }
+
+        // 最低点都不到该点的
+        if (Math.min(p1.y, p2.y) > point.y) {
+            continue;
+        }
+
+        const vertex: number =
+            (p2.x * point.y - p2.x * p1.y + p1.x * p2.y - p1.x * point.y) /
+            (p2.y - p1.y);
+
+        if (vertex > point.x) {
+            countNum = countNum + 1;
+        }
+    }
+
+    // 奇数在内部 偶数在外部
+    return countNum % 2 === 1;
+};
+
+/**
+ * winding-number算法
+ * @doc http://geomalgorithms.com/a03-_inclusion.html
+ */
+
+export const windingNumber: Function = (
+    p: IPoint,
+    polygon: IPointList,
+): boolean => {
+    const point: IPoint = isIPoint(p) ? p : p.point;
+    const pointList: IPointList = isArray(polygon) ? polygon : polygon.list;
+
+    return true;
+};
--- src/interface/point.ts
+++ src/interface/point.ts
@@ -1,8 +1,15 @@
 /**
  * @description point interface
  */
+// tslint:disable no-any no-unsafe-any
 
 export interface IPoint {
     x: number;
     y: number;
+}
+
+export type IPointList = IPoint[];
+
+export function isIPoint(v: any): v is IPoint {
+    return v.x !== undefined && v.y !== undefined;
 }
